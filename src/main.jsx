0bde8abaa21959a5d9532b3e970bb82e967091df
Gabriel Lima <gabriel58221@gmail.com>
2023-07-31 21:01:19 -0300

    implement values colors and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+
+    </ThemeProvider>
   </React.StrictMode>,
 )
